522b6670007c000a3598cf673ad1ded75ddfa84c
Gabriel Sinkin <gabe@milo.com>
2013-10-29 21:34:54 -0700

    Updated tests

--- test_gdd.py
+++ test_gdd.py
@@ -5,11 +5,8 @@ from gdd import calculate
 class TestGDD(unittest.TestCase):
 
     def test_gdd(self):
-        self.assertEquals(-32.5, calculate(high=23, low=12, base=50))
-        self.assertEquals(-38.5, calculate(13, 10, base=50))
-        self.assertEquals(-38.5, calculate(13, 10))
-        self.assertEquals(-38.5, calculate(13, 10))
-        self.assertEquals(-0.5, calculate(13, 10, 12))
+        self.assertEquals(0, calculate(high=23, low=12, base=50))
+        self.assertEquals(20, calculate(80, 60, 50))
 
 
 if __name__ == '__main__':
