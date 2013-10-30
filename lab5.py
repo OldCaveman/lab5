6c4d9a2f2222254435f4ee4ff581b68ab1f56e87
Alex Caven <apcaven@gmail.com>
2013-10-29 23:51:06 -0400

    lab5

--- lab5.py
+++ lab5.py
@@ -5,13 +5,19 @@ from gdd import calculate
 
 with open("Caven_Tullydata.csv") as infile:
     reader = csv.reader(infile)
-    high = max
+    high = -1000
+    low = 1000
+    last_day = None
     for row in reader:
         # TODO: extract the date's high and low temps
         date = datetime.datetime.strptime(row[0][0:10], "%Y-%m-%d")
-        try:
-            print date
-        except:
-            ValueError
+        temp = float(row[1])
+        if temp > high:
+            high = temp
+        if temp < low:
+            low = temp
+        if date != last_day:
+            value = calculate(high, low, base=50)
+            print value
         # Do something with calculate(high, low, base=10)
         pass
