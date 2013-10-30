279b84323401028e73bee7e7dae1c8920a5792d5
Alex Caven <apcaven@gmail.com>
2013-10-29 23:07:36 -0400

    date definition

--- lab5.py
+++ lab5.py
@@ -5,7 +5,12 @@ from gdd import calculate
 
 with open("Caven_Tullydata.csv") as infile:
     reader = csv.reader(infile)
+    old_data = None
+    high =
+    low = 
     for row in reader:
         # TODO: extract the date's high and low temps
+        date = datetime.date(2006, 1, 1)
+        if date != 
         # Do something with calculate(high, low, base=10)
         pass
