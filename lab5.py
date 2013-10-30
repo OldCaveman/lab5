d83c25a2f58918d96c0dd7023a7907da3e955eb2
Alex Caven <apcaven@gmail.com>
2013-10-29 23:12:37 -0400

    corrected lab5 error

--- lab5.py
+++ lab5.py
@@ -5,12 +5,10 @@ from gdd import calculate
 
 with open("Caven_Tullydata.csv") as infile:
     reader = csv.reader(infile)
-    old_data = None
-    high =
-    low = 
+    high = max
     for row in reader:
         # TODO: extract the date's high and low temps
-        date = datetime.date(2006, 1, 1)
-        if date != 
+        date = datetime.date(row[0])
+        print date
         # Do something with calculate(high, low, base=10)
         pass
